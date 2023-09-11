cc791ac970f07aebdc527c2518a56173359f65ef
seming2 <131343462+seming2@users.noreply.github.com>
2023-09-11 23:25:22 +0900

    Add files via upload

---
+++ 플래티어/js/script.js
@@ -0,0 +1,116 @@
+$(function(){
+    $('header').mouseover(function(){
+        $('header').addClass('on')
+        $(this).parent('.header_wrap').addClass('on')
+        $(this).find('.logo').addClass('on')
+        $(this).find('.lang').addClass('on')
+        $(this).find('.text_box').addClass('on')
+
+
+        $(this).find('.lnb').css({display:'flex'})
+
+    })
+
+
+
+
+    $('header').mouseout(function(){
+        $('header').removeClass('on')
+        $(this).find('.logo').removeClass('on')
+        $(this).find('.lang').removeClass('on')
+        $(this).find('.text_box').removeClass('on')
+        $(this).find('.lnb').css({display:'none'})
+    })
+
+    let liH = $('.lnb').height();
+    
+    $('.lnb_wrap .lnb li').mouseover(function(){
+      $(this).addClass('on');
+    })
+    $('.lnb_wrap .lnb li').mouseleave(function(){
+      $(this).removeClass('on');
+    })
+
+
+  
+    let comPanyLnb = $('.lnb_wrap li a');
+    let pageUrl = window.location.href
+    $(comPanyLnb).each(function(){
+      let lnbHref = $(this).attr('href');
+        if(pageUrl.indexOf(lnbHref) > 0 ){
+            $(this).css({color:'#00acee'});
+            $(this).parent().addClass('active')
+            
+        }
+       
+    })
+
+    $('.lnb').each(function(){
+      if( liH == 274){
+        $('.lnb').css({height:274})
+      }
+  
+    })
+
+
+    let hisToryBtn = $('.contents_box li .ch1 .more');
+    let hisToryText = $('.history .history_wrap .contents_box .ch2');
+    let hisToryBtnUp = $('.contents_box li .ch2 .more');
+
+    hisToryBtn.click(function(){
+      hisToryText.show();
+      console.log(hisToryText)
+    })
+
+    hisToryBtnUp.click(function(){
+      hisToryText.hide();
+      console.log(hisToryText)
+    })
+
+
+
+
+let platformBtn = $('.cm_platform .flatform_gnb li');
+
+   platformBtn.click(function(){
+    $(this).addClass('active').siblings().removeClass('active')
+
+    let formBox = $(this).parents('.flatform_gnb');
+
+    let showBox =  formBox.siblings('.con_none')
+
+    // formBox.siblings('.con_none').show();
+    console.log(formBox)
+
+    return false;
+   })
+
+
+
+
+
+
+
+
+
+var bullet = ['1번', '2번', '3번'];
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<div class="' + className + '"><span>' + (bullet[index]) + '</span></div>';
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+
+
+
+
+})
